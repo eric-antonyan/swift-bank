41ef65880fa7bbfedeca773f419012f6e3ec9fec
erik_antonyan <erikantonian@gmail.com>
2024-07-17 18:18:49 +0400

    swift-bank

--- client/src/models/config.model.js
+++ client/src/models/config.model.js
@@ -21,4 +21,4 @@ function getLocalIP(callback) {
 }
 
 // Use the function to get the local IP address and display it
-export const api = "https://f94319ab-55ce-40ed-94f0-bdc64da8f6a6-00-39tt120ugwq6i.sisko.replit.dev:3001";
+export const api = "http://localhost:6006";
--- server/routes/check.router.js
+++ server/routes/check.router.js
@@ -1,37 +1,46 @@
-const { Router } = require("express")
-const router = Router()
+const { Router } = require("express");
+const router = Router();
 const users = require("../models/users.js");
 const jwt = require("jsonwebtoken");
+const { server_passcode } = require("../models/config.model.js");
 
 router.post("/email", async (req, res) => {
-    if (req.headers.authorization === process.env.SERVER_PASSCODE) {
-        const { email } = req.body
-        const isExist = await users.findOne({ email })
+    console.log(req.headers.authorization === process.env.SERVER_PASSCODE);
+    if (req.headers.authorization === server_passcode) {
+        const { email } = req.body;
+        const isExist = await users.findOne({ email });
         if (isExist) {
-            res.json({ success: false, message: "This email (" + email + ") is exist", reqData: req.body })
+            res.json({
+                success: false,
+                message: "This email (" + email + ") is exist",
+                reqData: req.body,
+            });
         } else {
-            res.json({ success: true, auth: req.headers.authorization })
+            res.json({ success: true, auth: req.headers.authorization });
         }
     } else {
-        res.send({ message: "Auth not found" })
+        res.send({ message: "Auth not found" });
     }
-})
+});
 
-router.post('/')
+router.post("/");
 
 router.post("/phone", async (req, res) => {
-    if (req.headers.authorization === process.env.SERVER_PASSCODE) {
-        const { phoneNumber } = req.body
-        const isExist = await users.findOne({ phoneNumber })
+    if (req.headers.authorization === server_passcode) {
+        const { phoneNumber } = req.body;
+        const isExist = await users.findOne({ phoneNumber });
         if (isExist) {
-            res.json({ success: false, message: "This phone (" + phoneNumber + ") is exist", reqData: req.body })
+            res.json({
+                success: false,
+                message: "This phone (" + phoneNumber + ") is exist",
+                reqData: req.body,
+            });
         } else {
-            res.json({ success: true, auth: req.headers.authorization })
+            res.json({ success: true, auth: req.headers.authorization });
         }
     } else {
-        res.send({ message: "Auth not found" })
+        res.send({ message: "Auth not found" });
     }
-})
+});
 
-
-module.exports = router
+module.exports = router;
